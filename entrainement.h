f48cddd1affcaa307d23afad2eaafb09793f0375
Mohamed <momo.azerty78@gmail.com>
2024-05-23 00:17:22 +0200

    commentaire try.h

--- entrainement.h
+++ entrainement.h
@@ -4,36 +4,28 @@
 #include "structure.h"
 
 
-
+//Lecture d'un entraiement
 entrainement** lis_un_fichier_d_entrainement(const char* nomFichier, int* nb_entrainement );
 
-
+//Affiche le tableau des entrainement
 void printArrayOfentrainement(entrainement** TAB_DATA, int nbEntrainement);
 
+//Affiche le tableau d'un entrainement
 void print1entrainement(entrainement* p_entrainement, int i);
 
+//Ajout de l'entrainement dans le fichier
 void writeArrayOftrainingTOfile(const char* nomFichier, entrainement** tab_entrainement, int  nbEntrainement);
 
-
+//Ajouter un nouvel entrainement
 void addNewEntrainementSaisiUser(entrainement** tab_entrainement, int* nbEntrainement, char*epeuve);
 
 
-// ecris tous les entrainement d'un athlete dans son fichier
-//void writeAllEntrainementTOfile(const char* nomAthlete, entrainement** tab_entrainement, int nbEntrainement);
-
-// creation nouvel entrainement
-// return pointeur dessus
+// Creation nouvel entrainement
 entrainement* getNewEntrainement(date la_date, char* epreuve, performance perf, int position_relais);
 
 
-// ajout du nouvel entrainement dans le tab de tous les entrainements
+// Ajout du nouvel entrainement dans le tab de tous les entrainements ainsi que dans le fichier
 void addNewEntrainementDansTab(entrainement** tab_entrainement, int* nbEntrainement, entrainement* p_new_entrainement);
-
-
-// ajout du nouvel entrainement dans le tab de tous les entrainements !
-// 1. lit le fichier de l'athlete
-// 2. ajout l'entrainement
-// 3. reecris le fichier
 void addNewEntrainementDansFichier(const char* nomAthlete, entrainement* p_new_entrainement);
 
 
